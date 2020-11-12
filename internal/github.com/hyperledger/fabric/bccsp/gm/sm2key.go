87e99f7cead50b3d8a29558b33716e88db414a23
songweiqi <songweiqi1994@sina.com>
2020-11-11 22:47:58 -0800

    1.priv_key bytes

--- internal/github.com/hyperledger/fabric/bccsp/gm/sm2key.go
+++ internal/github.com/hyperledger/fabric/bccsp/gm/sm2key.go
@@ -18,7 +18,6 @@ package gm
 import (
 	"crypto/elliptic"
 	"crypto/sha256"
-	"errors"
 	"fmt"
 
 	"github.com/VoneChain-CS/fabric-sdk-go-gm/internal/github.com/hyperledger/fabric/bccsp"
@@ -32,7 +31,7 @@ type gmsm2PrivateKey struct {
 // Bytes converts this key to its byte representation,
 // if this operation is allowed.
 func (k *gmsm2PrivateKey) Bytes() (raw []byte, err error) {
-	return nil, errors.New("Not supported.")
+	return sm2.MarshalSm2UnecryptedPrivateKey(k.privKey)
 }
 
 // SKI returns the subject key identifier of this key.
